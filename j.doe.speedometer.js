215c0ae36e9ffa6836076341cf5b3e4844272a3e
Daniel Önnerby <onnerby@gmail.com>
2012-06-20 15:12:18 +0200

    Speedometer working

--- j.doe.speedometer.js
+++ j.doe.speedometer.js
@@ -16,9 +16,72 @@
 			var p	= Raphael($(this).get(0),opts.width,opts.height);
 			var cX	= opts.width/2;
 			var cY	= opts.height/2;
+			var r	= opts.width/2-1;
+			var angle	= Math.PI*0.66;
+
+			// Draw background
+			p.path(
+				"M"+(cX-r*Math.sin(angle))+
+				","+(cY-r*Math.cos(angle))+
+				"A"+r+
+				","+r+
+				" 0 1 1"+
+				" "+(cX-r*Math.sin(-angle))+
+				" "+(cY-r*Math.cos(-angle))+
+				"A"+1.4*r+
+				","+r+
+				" 0 0 0"+
+				" "+(cX-r*Math.sin(angle))+
+				" "+(cY-r*Math.cos(angle))+
+				"z"
+				).attr({
+					fill:"#fff",
+					stroke:"#d7d7d7",
+					'stroke-width':1
+				});
+
+			angle	= Math.PI*0.65;
 			
-			p.path("M"+cX+","+cY+"A"+(cX-opts.margin)+","+(cY-opts.margin)+" 0 0 0 20 20").attr({fill:"#fc9",stroke:"#006",'stroke-width':5});
+			var currentAngle	= angle;
+			var valPath	= false;
 			
+			valText	= p.text(cX,cY-r*0.4, opts.value+opts.unit ).attr({
+				"font-family":"Arial",
+				"font-size":"12px",
+				"font-weight":"bold",
+				fill:opts.barColor
+				});
+			
+			var pathInterval=setInterval( function(){
+				// Draw value
+				r	= opts.width/2-opts.margin-opts.thickness*0.5;
+				var val		= (opts.value-opts.min)/(opts.max-opts.min);
+				if(val>2){ val=2.0; }
+				var valAngle	= angle*2*(1-val*0.5)-angle;
+				
+				if(currentAngle!=valAngle){
+					currentAngle	= currentAngle+(valAngle-currentAngle)*0.1;
+					if(Math.round(currentAngle*100)==Math.round(valAngle*100)){
+						currentAngle=valAngle;
+						clearInterval(pathInterval);
+					}
+					var largeCircle	= ((currentAngle-angle)*0.5 < (-Math.PI/2))?1:0;
+					if(valPath){ valPath.remove();}
+					valPath	= p.path(
+						"M"+(cX-r*Math.sin(angle))+
+						","+(cY-r*Math.cos(angle))+
+						"A"+r+
+						","+r+
+						" 0 "+largeCircle+" 1"+
+						" "+(cX-r*Math.sin(currentAngle))+
+						" "+(cY-r*Math.cos(currentAngle))
+						).attr({
+							fill:"none",
+							stroke:opts.barColor,
+							'stroke-width':opts.thickness
+						});
+				}
+			},30);				
 		});
 		
 	};
@@ -29,9 +92,11 @@
 		max: 100,
 		value: 0,
 		unit: '%',
-		width: 200,
-		height: 200,
-		margin:2
+		width: 135,
+		height: 135,
+		thickness: 6,
+		margin:4,
+		barColor: "#79bfa8"
 	};
 	
 })(jQuery);
